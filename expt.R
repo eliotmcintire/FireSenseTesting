4fc5ec2d00e3ff61095f63aa160853d523a73c8c
Eliot McIntire <eliotmcintire@gmail.com>
2026-02-17 14:22:11 -0800

    Delete folder

--- expt.R
+++ expt.R
@@ -10,7 +10,7 @@ remotes::install_github("PredictiveEcology/SpaDES.project@cacheRequire")
 # unlink(dir("logs", full.names = TRUE)) ; source("expt.R")
 suppressWarnings(rm(.ELFind)) # This is a precaution as this may exist if there is a failure below; and this is rerun
 # pkgload::load_all("~/GitHub/reproducible/");
-# pkgload::load_all("~/GitHub/SpaDES.project/");
+pkgload::load_all("~/GitHub/SpaDES.project/");
 
 outs <- SpaDES.project::preRunSetupProject(file = "global.R", upTo = "params")
 outs$modules <- grep("ELFs", outs$modules, value = TRUE)
@@ -22,18 +22,18 @@ sim <- SpaDES.core::simInitAndSpades2(outs) |>
 .ELFinds <- names(sim$ELFs$rasCentered)
 
 # If you can run them in parallel on the same linux machine:
-
-prepInputsFSURL <- "https://drive.google.com/drive/folders/1X9-mRjyLMNpgkP_cfqhbr_AQEPOsVCHf"
-gdLs <- googledrive::drive_ls(prepInputsFSURL)
-fireSenseParamsRDS <- sim@params$.globals$spreadFitFilename
-remoteFile <- gdLs[gdLs$name %in% fireSenseParamsRDS,]
-digRemote <- remoteFile$drive_resource[[1]]$md5Checksum
-gdMeta <- googledrive::drive_download(remoteFile, 
-                            path = file.path("/home/emcintir/GitHub/FireSenseTesting/inputs", remoteFile$name),
-                            overwrite = TRUE) |> 
-  reproducible::Cache(.cacheExtra = digRemote)
-aa <- readRDS(gdMeta$local_path)
-aa
+# 
+# prepInputsFSURL <- "https://drive.google.com/drive/folders/1X9-mRjyLMNpgkP_cfqhbr_AQEPOsVCHf"
+# gdLs <- googledrive::drive_ls(prepInputsFSURL)
+# fireSenseParamsRDS <- sim@params$.globals$spreadFitFilename
+# remoteFile <- gdLs[gdLs$name %in% fireSenseParamsRDS,]
+# digRemote <- remoteFile$drive_resource[[1]]$md5Checksum
+# gdMeta <- googledrive::drive_download(remoteFile, 
+#                             path = file.path("/home/emcintir/GitHub/FireSenseTesting/inputs", remoteFile$name),
+#                             overwrite = TRUE) |> 
+#   reproducible::Cache(.cacheExtra = digRemote)
+# aa <- readRDS(gdMeta$local_path)
+# aa
 
 .reps <- 1
 expt <- expand.grid(.ELFind = .ELFinds, .rep = .reps, stringsAsFactors = FALSE)
